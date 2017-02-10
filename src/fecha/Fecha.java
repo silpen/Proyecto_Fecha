1a82ee8c1d3159c0b7c90b380f51db6cd651b0de
203p27 <203p27@203p28-PC>
2017-02-10 09:37:54 +0100

    primer punto

---
+++ src/fecha/Fecha.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fecha;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author 203p27
+ */
+public class Fecha {
+    int dia;
+    int mes;
+    int ano;
+
+    public Fecha(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    public Fecha() {
+        Calendar fechaHoy=new GregorianCalendar();
+        dia= fechaHoy.get(Calendar.DAY_OF_MONTH);
+        mes=fechaHoy.get(Calendar.MONTH)+1;
+        ano=fechaHoy.get(Calendar.YEAR);
+    }
+    
+    
+}
